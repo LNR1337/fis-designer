f56a531ae3441ec4485e77fefa3e1a5ae4387e03
Paweł Szydło <pawelszydlo@gmail.com>
2021-12-29 22:14:30 +0100

    Title service.

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,12 +1,14 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
+import {Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
   styleUrls: ['./pages.component.scss'],
 })
-export class PagesComponent implements OnInit {
-  constructor() {}
+export class PagesComponent {
+  constructor(titleService: Title) {
+    titleService.setTitle('FIS-Designer - About');
+  }
 
-  ngOnInit(): void {}
 }
--- src/app/workspace/workspace.component.ts
+++ src/app/workspace/workspace.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-workspace',
@@ -6,5 +7,7 @@ import {Component} from '@angular/core';
   styleUrls: ['./workspace.component.scss'],
 })
 export class WorkspaceComponent {
-  constructor() {}
+  constructor(titleService: Title) {
+    titleService.setTitle('FIS-Designer');
+  }
 }
